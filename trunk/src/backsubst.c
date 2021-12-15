62ef96d2793ef6ae065eaa6ad5183a7ecac1f6ff
gardyjam <gardyjas.monika1@gmail.com>
2021-12-15 20:56:32 +0100

    Poprawiona wersja pliku backsubst.c

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -5,24 +5,31 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 
-// PIERWSZA WERSJA - PODSTAWOWA (do poprawy)
-// nie jest przystosowana do przypadku wprowadzenia błędnych danych 
-// nie wyświetla żadnych komuniatów o błędzie, nie ma przypadku zwracania 1 lub 2
-
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 	int i,j; // zmienne pomocnicze
 	double sum;
+	int r = mat->r;
+	int c = mat->c;
 
-	// oststanie równanie - osobny przypadek
-	x->data[r][0] = b->data[r][0]/mat->data[r][c];
-
+	// sprawdzam, czy wymiary macierzy są poprawne
+	if( mat->r != b->r || mat->r != mat->c )
+		return 2;
+	
+	// ostatnie równanie - osobny przypadek
+	if( mat->data[r][c] != 0 ) 
+		x->data[r][0] = b->data[r][0]/mat->data[r][c];
+	else 
+		return 1;
 	// pozostałe równania - od równania r-1 do 0
 	for( i = r-1; i >= 0; i-- ) {
 		sum = 0;
-		for( j = i+1; j <= r; j++ ) {
+		for( j = i+1; j <= c; j++ ) {
 			sum += x->data[j][0] * mat->data[i][j];
 		}
-		x->data[i][0] = (b->data[i][0]-sum) mat->data[i][i];
+		if( mat->data[i][i] != 0 )
+			x->data[i][0] = (b->data[i][0]-sum)/ mat->data[i][i];
+		else
+			return 1;
 	}
 
 	return 0;
